de7a4885287f7807ad086ff636ba1f0b5b53c35e
wongkeaw <oa.wongkeaw@gmail.com>
2019-02-27 22:19:26 +0700

    Initial Commit

---
+++ ONG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ONG
+//
+//  Created by ongarj wongkeaw on 2/27/19.
+//  Copyright © 2019 ongarj wongkeaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
